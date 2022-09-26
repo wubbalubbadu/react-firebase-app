d50ed63558dc5534e876e8d61ad88ed7a6391bb0
wubbalubbadu <wavgoddbye@gmail.com>
2022-09-26 13:54:56 -0500

    init page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <h1>{schedule.title}</h1>
+);
 
 export default App;
